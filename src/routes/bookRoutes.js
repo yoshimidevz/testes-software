e222010a64104cc9f3537adb0e9423fa35452ed9
yoshimidevz <yoshimidev1@gmail.com>
2026-03-26 09:36:15 -0300

    feat: adicionando rotas de livro e criando rotas de usuario

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,10 +1,13 @@
 const {Router} = require('express');
-const {create, get} = require('../controllers/bookController');
+const {create, get, getById, deletar, atualizar} = require('../controllers/bookController');
 
 const router = Router();
 
 router.post("/", create);
 router.get("/", get);
+router.delete('/:id', deletar);
+router.get('/:id', getById);
+router.put('/:id', atualizar);
 
 
 module.exports = router;
