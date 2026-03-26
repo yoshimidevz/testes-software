4a2e786c24000e39bc6393a397d36589d2ecace5
yoshimidevz <yoshimidev1@gmail.com>
2026-03-26 09:35:32 -0300

    feat: criando migration de table books e criando env example

---
+++ src/database/migrations/20260318031746-create-books.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('books', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      author: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('books');
+  }
+};
