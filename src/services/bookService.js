91e8f0250107701e0d40d84fd47b707981e37690
yoshimidevz <yoshimidev1@gmail.com>
2026-03-26 09:34:45 -0300

    feat: implementando crud deletar, update e get all books

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -1,15 +1,41 @@
-const books = [];
+const Book = require('../models/book');
 
-const createBook = (title, author) => {
-    const book = { id: books.length + 1, title, author};
-    books.push(book)
-    return book;
+const createBook = async (title, author) => {
+    const book = await Book.create({title, author});
+    return {
+        id: book.id,
+        title: book.title,
+        author: book.author,
+    };
 }
 
 const getBookById = async (id) => {
     return await Book.findByPk(id);
 }
 
+const deleteBook = async (id) => {
+    const book = await getBookById(id);
+    if (!book) {
+        throw new Error('Book not found');
+    }
+    await book.destroy();
+}
+
+const updateBook = async (id, title, author) => {
+    const book = await getBookById(id);
+    if (!book) {
+        throw new Error('Book not found');
+    }
+    book.title = title;
+    book.author = author;
+    await book.save();
+    return book;
+}
+
+const getAllBooks = async () => {
+    return await Book.findAll();
+}
 
 
-module.exports = { createBook, getBookById };
+
+module.exports = { createBook, getBookById, deleteBook, updateBook, getAllBooks };
