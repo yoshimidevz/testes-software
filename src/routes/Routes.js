a95dcd88a6e3b46145f4f93cf64f2c8b7f449e95
yoshimidevz <yoshimidev1@gmail.com>
2026-03-26 22:22:15 -0300

    feat: add user routes to main router

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,8 +1,10 @@
 const {Router} = require('express');
 const bookRoutes = require('./bookRoutes');
+const userRoutes = require('./userRoutes');
 
 const router = Router();
 
 router.use('/books', bookRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
