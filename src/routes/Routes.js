8d4b439bddc5da2adb7f78b1e77539c16abbb6a4
yoshimidevz <yoshimidev1@gmail.com>
2026-04-09 14:49:24 -0300

    refactor: update routes.js

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,11 +2,13 @@ const {Router} = require('express');
 const bookRoutes = require('./bookRoutes');
 const userRoutes = require('./userRoutes');
 const loanRoutes = require('./loanRoutes');
+const fineRoutes = require('./fineRoutes');
 
 const router = Router();
 
 router.use('/books', bookRoutes);
 router.use('/users', userRoutes);
 router.use('/loans', loanRoutes);
+router.use('/fines', fineRoutes);
 
 module.exports = router;
